5ec134e80e460118ae84f4dfdceba5e1c6ae9fa0
DAVID PC-CAJA <davidortiz25122010@gmail.com>
2022-04-27 09:38:30 -0400

    cambios de estructura

--- public/js/app.js
+++ public/js/app.js
@@ -51545,8 +51545,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\JuegosySofwareinstalado\laragon\www\devjobs\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\JuegosySofwareinstalado\laragon\www\devjobs\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\devjobs\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\devjobs\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
